2d659f19ab032ec99c726e6c9dc4f053f937c31c
criscabe <55502955+criscabe@users.noreply.github.com>
2023-03-06 16:26:49 +0100

    Analyze simulated reduced data

--- code/2_lines_analysis.py
+++ code/2_lines_analysis.py
@@ -35,28 +35,28 @@ if __name__ == "__main__":
     parser.add_argument('target',
                         help="Name of the target")
     ##
-    parser.add_argument("--nameEL", help = "Name of the emission line", type=str)
+    parser.add_argument('-LINE', help = "Name of the emission line", type=str)
     parser.add_argument("--analyze", help = "Analyze the corresponding line",
                         action="store_true")
     parser.add_argument("--plots", help = "Create and save the figures",
                         action="store_true")
     ##
-    parser.add_argument('-f', '--method', default=0, choices=[0,1,2], metavar='FITTING FUNCTION (0,1,2)', help='Fitting function (0=gauss_hermite, 1=gauss, 2=double_gauss)', type=int)
-    parser.add_argument('-S', '--limsnr', default=5, metavar='MINIMUM S/N', help='Mininum Signal-to-noise ratio in each spaxel', type=float)
-    parser.add_argument('-w', '--ctwl', metavar='LINE CENTRAL WAVELENGTH', help='Central rest-frame wavelength for line (AA)',
+    parser.add_argument('-f', default=0, choices=[0,1,2], help='Fitting function (0=gauss_hermite, 1=gauss, 2=double_gauss)', type=int)
+    parser.add_argument('-S', default=5, help='Mininum Signal-to-noise ratio in each spaxel', type=float)
+    parser.add_argument('-w', help='Central rest-frame wavelength of the emission line (AA)',
                         type=float)
-    parser.add_argument('-z', '--redshift', metavar='REDSHIFT', help='Redshift for target and catalog lines', type=float)
-    parser.add_argument('-LW1', '--lcut1', metavar='LOWER WAVELENGTH - LINE', help='Lower rest-frame wavelength for line (AA)',
+    parser.add_argument('-z', help='Redshift of the target', type=float)
+    parser.add_argument('-LW1', help='Lower rest-frame wavelength (AA) for the emission-line fitting',
                         type=float)
-    parser.add_argument('-LW2', '--lcut2', metavar='UPPER WAVELENGTH - LINE', help='Upper rest-frame wavelength for line (AA)',
+    parser.add_argument('-LW2', help='Upper rest-frame wavelength (AA) for the emission-line fitting',
                         type=float)
-    parser.add_argument('-CW1', '--ccut1', metavar='LOWER WAVELENGTH - CONT', help='Lower rest-frame wavelength for cont. (AA)',
+    parser.add_argument('-CW1', help='Lower rest-frame wavelength (AA) for the continuum fitting',
                         type=float)
-    parser.add_argument('-CW2', '--ccut2', metavar='UPPER WAVELENGTH - CONT', help='Upper rest-frame wavelength for cont. (AA)',
+    parser.add_argument('-CW2', help='Upper rest-frame wavelength (AA) for the continuum fitting',
                         type=float)
-    parser.add_argument('-PW1', '--pcut1', metavar='LOWER WAVELENGTH - PLOT', help='Lower (observed) wavelength for plot (AA)',
+    parser.add_argument('-PW1', help='Lower (observed) wavelength (AA) for the plot',
                         type=float)
-    parser.add_argument('-PW2', '--pcut2', metavar='UPPER WAVELENGTH - PLOT', help='Upper (observed) wavelength for plot (AA)',
+    parser.add_argument('-PW2', help='Upper (observed) wavelength (AA) for the plot',
                         type=float)
 
     args = parser.parse_args()
@@ -71,21 +71,22 @@ if __name__ == "__main__":
     VPH = str(args.VPH)
     target = str(args.target)
     
-    f = args.method
-    snr = args.limsnr
-    lw1= args.lcut1
-    lw2= args.lcut2
-    cw1 = args.ccut1
-    cw2 = args.ccut2
-    pw1 = args.pcut1
-    pw2 = args.pcut2
-    w = args.ctwl
-    z = args.redshift
+    f = args.f
+    snr = args.S
+    w = args.w
+    z = args.z
+    lw1= args.LW1
+    lw2= args.LW2
+    cw1 = args.CW1
+    cw2 = args.CW2
+    pw1 = args.PW1
+    pw2 = args.PW2
+
     
     ###########
         
     
-    name = str(args.nameEL)
+    name = str(args.LINE)
     print('.............................')
     print('--------Analyze the ' + str(name) + ' line--------')
     print('.............................')
